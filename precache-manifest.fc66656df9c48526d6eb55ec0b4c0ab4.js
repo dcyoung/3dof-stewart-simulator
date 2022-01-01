6cdef3cfaaa617d10ba55f8ecb34984cf2d2f61b
dcyoung <david@questionablyartificial.com>
2021-12-31 17:57:43 -0800

    Updates

---
+++ precache-manifest.fc66656df9c48526d6eb55ec0b4c0ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0250091bbffe0cbcf5256db0fb6cb91f",
+    "url": "/3dof-stewart-simulator/index.html"
+  },
+  {
+    "revision": "48630c647628d80d319d",
+    "url": "/3dof-stewart-simulator/static/css/2.6cbc0fd3.chunk.css"
+  },
+  {
+    "revision": "d32b9adf70f4423ee4b9",
+    "url": "/3dof-stewart-simulator/static/css/main.d9342892.chunk.css"
+  },
+  {
+    "revision": "48630c647628d80d319d",
+    "url": "/3dof-stewart-simulator/static/js/2.a001b243.chunk.js"
+  },
+  {
+    "revision": "d32b9adf70f4423ee4b9",
+    "url": "/3dof-stewart-simulator/static/js/main.aadac10c.chunk.js"
+  },
+  {
+    "revision": "d90a81365809694be13d",
+    "url": "/3dof-stewart-simulator/static/js/runtime~main.cd19b37c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a419dc4adc2be566d028fc8d830a4c82.js"
+  "/3dof-stewart-simulator/precache-manifest.fc66656df9c48526d6eb55ec0b4c0ab4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/3dof-stewart-simulator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
